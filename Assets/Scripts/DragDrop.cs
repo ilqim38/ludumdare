da8dd5cdd85f1c6dfca935cd63a593bf0b4d8489
ilqim <210159408+ilqim38@users.noreply.github.com>
2025-10-05 01:14:25 +0300

    v0.1 fixed file

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Sürükleme Başladı!");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Sürükleniyor!");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Sürükleme Bitti!");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Tıklandın!");
+    }
+}
